f58f2463ebfe2204c110b37fe4d764dfc4d658f9
Thomas Wozniakowski <thomas.wozniakowski@masabi.com>
2017-12-22 11:01:28 +0000

    Made various system ports configurable via environment overrides

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ docker-flink
 Docker packaging for Apache Flink
 
 Use `add-version.sh` to rebuild the Dockerfiles and all variants for a
-particular Flink release release. Before running this, you must first delete
+particular Flink release. Before running this, you must first delete
 the existing release directory.
 
     usage: ./add-version.sh -r flink-release -f flink-version
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,6 +20,11 @@
 
 # If unspecified, the hostname of the container is taken as the JobManager address
 JOB_MANAGER_RPC_ADDRESS=${JOB_MANAGER_RPC_ADDRESS:-$(hostname -f)}
+JOB_MANAGER_RPC_PORT=${JOB_MANAGER_RPC_PORT:-$(6123)}
+JOB_MANAGER_WEB_PORT=${JOB_MANAGER_WEB_PORT:-$(8081)}
+TASK_MANAGER_NUMBER_OF_TASK_SLOTS=${TASK_MANAGER_NUMBER_OF_TASK_SLOTS:-$(grep -c ^processor /proc/cpuinfo)}
+BLOB_SERVER_PORT=${BLOB_SERVER_PORT:-$(6124)}
+QUERY_SERVER_PORT=${QUERY_SERVER_PORT:-$(6125)}
 
 drop_privs_cmd() {
     if [ -x /sbin/su-exec ]; then
@@ -31,30 +36,32 @@ drop_privs_cmd() {
     fi
 }
 
+inject_config_overrides() {
+    # Find and replace existing config
+    sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" "$FLINK_HOME/conf/flink-conf.yaml"
+    sed -i -e "s/jobmanager.rpc.port: 6123/jobmanager.rpc.port: ${JOB_MANAGER_RPC_PORT}/g" "$FLINK_HOME/conf/flink-conf.yaml"
+    sed -i -e "s/jobmanager.web.port: 8081/jobmanager.web.port: ${JOB_MANAGER_WEB_PORT}/g" "$FLINK_HOME/conf/flink-conf.yaml"
+    sed -i -e "s/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: $TASK_MANAGER_NUMBER_OF_TASK_SLOTS/g" "$FLINK_HOME/conf/flink-conf.yaml"
+    # Inject new previously unspecified config
+    echo "blob.server.port: ${BLOB_SERVER_PORT}" >> "$FLINK_HOME/conf/flink-conf.yaml"
+    echo "query.server.port: ${QUERY_SERVER_PORT}" >> "$FLINK_HOME/conf/flink-conf.yaml"
+
+    echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
+}
+
 if [ "$1" = "help" ]; then
     echo "Usage: $(basename "$0") (jobmanager|taskmanager|local|help)"
     exit 0
 elif [ "$1" = "jobmanager" ]; then
     echo "Starting Job Manager"
-    sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" "$FLINK_HOME/conf/flink-conf.yaml"
-    echo "blob.server.port: 6124" >> "$FLINK_HOME/conf/flink-conf.yaml"
-    echo "query.server.port: 6125" >> "$FLINK_HOME/conf/flink-conf.yaml"
-
-    echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
+    inject_config_overrides
     exec $(drop_privs_cmd) flink "$FLINK_HOME/bin/jobmanager.sh" start-foreground cluster
 elif [ "$1" = "taskmanager" ]; then
-    TASK_MANAGER_NUMBER_OF_TASK_SLOTS=${TASK_MANAGER_NUMBER_OF_TASK_SLOTS:-$(grep -c ^processor /proc/cpuinfo)}
-
-    sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" "$FLINK_HOME/conf/flink-conf.yaml"
-    sed -i -e "s/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: $TASK_MANAGER_NUMBER_OF_TASK_SLOTS/g" "$FLINK_HOME/conf/flink-conf.yaml"
-    echo "blob.server.port: 6124" >> "$FLINK_HOME/conf/flink-conf.yaml"
-    echo "query.server.port: 6125" >> "$FLINK_HOME/conf/flink-conf.yaml"
-
-    echo "Starting Task Manager"
-    echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
+    inject_config_overrides
     exec $(drop_privs_cmd) flink "$FLINK_HOME/bin/taskmanager.sh" start-foreground
 elif [ "$1" = "local" ]; then
     echo "Starting local cluster"
+    inject_config_overrides
     exec $(drop_privs_cmd) flink "$FLINK_HOME/bin/jobmanager.sh" start-foreground local
 fi
 
